369604df07c2c0104f377d7aa2fc8c8d2b712179
smgtoxa <smgtoxa@gmail.com>
2020-05-11 23:40:45 -0700

    signup

--- prodhunt/users/views.py
+++ prodhunt/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
-# Create your views here.
+def signup(requests):
+    if requests.method == "POST":
+        if requests.POST["password1"] == requests.POST["password2"]:
+            try:
+                user = User.objects.get(username=requests.POST['username'])
+                return render(requests, "users/signup.html", {'error':'username has already been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(requests.POST["username"], password = requests.POST["password1"])
+                auth.login(requests,user)
+                return redirect("home")
+    else:
+        return render(requests, "users/signup.html")
+
+def login(requests):
+    return render(requests, "users/login.html")
+# TODO need to add page 
+def logout(requests):
+    return render(requests, "users/logout.html")
