d98a3386291b971958a932962d15c98aa15f2452
smgtoxa <smgtoxa@gmail.com>
2020-05-30 21:22:26 -0700

    complete

--- prodhunt/products/views.py
+++ prodhunt/products/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Product
 from django.utils import timezone
-def home(requests):
-    return render(requests, "products/home.html")
 
-@login_required
+def home(requests):
+    products = Product.objects
+    return render(requests, "products/home.html", {"products": products})
+
+@login_required(login_url='/users/signup')
 def create(requests):
     if requests.method == 'POST':
         if requests.POST['title'] and requests.POST['body'] and requests.POST['url']  and requests.FILES['icon'] and requests.FILES['image']:
                 product = Product()
                 product.title = requests.POST['title']
                 product.body = requests.POST['body']
-                if requests.POST['url'].lower.startswith('http://') or requests.POST['url'].lower.startswith('https://'): 
+                if str(requests.POST['url'].lower()).startswith('http://') or str(requests.POST['url'].lower()).startswith('https://'): 
                     product.urs = requests.POST['url']
                 else:
                     product.urs = 'http://' + requests.POST['url']
@@ -31,4 +33,12 @@ def create(requests):
 def detail(requests, product_id):
     product = get_object_or_404(Product, pk=product_id)
     return render(requests, 'products/detail.html', {'product':product})
-    
+
+@login_required(login_url='/users/signup')    
+def upvote(requests, product_id):
+    if requests.method == "POST":
+        product = get_object_or_404(Product, pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return redirect('/products/' + str(product.id))     
+
